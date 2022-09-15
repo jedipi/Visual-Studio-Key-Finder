50e90c83b0ac7799145518f26b5375041618a244
Kindeman <jedipi@hotmail.com>
2022-09-15 13:03:38 +1000

    add support for Visual Studio 2019

--- src/VsKeyFinder/ProductData.cs
+++ src/VsKeyFinder/ProductData.cs
@@ -10,6 +10,20 @@ namespace VsKeyFinder
             {
                 new Product()
                 {
+                    Name = "Visual Studio 2019 Enterprise",
+                    Guid = "41717607-F34E-432C-A138-A3CFD7E25CDA",
+                    Code = "09260",
+                    Key = ""
+                },
+                new Product()
+                {
+                    Name = "Visual Studio 2019 Professional",
+                    Guid = "41717607-F34E-432C-A138-A3CFD7E25CDA",
+                    Code = "09262",
+                    Key = ""
+                },
+                new Product()
+                {
                     Name = "Visual Studio 2022 Enterprise",
                     Guid = "1299B4B9-DFCC-476D-98F0-F65A2B46C96D",
                     Code = "09660",
