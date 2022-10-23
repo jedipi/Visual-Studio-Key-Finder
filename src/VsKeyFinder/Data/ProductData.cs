c612f6d8ba8395a431a82a199bba735794ae218c
Kindeman <jedipi@hotmail.com>
2022-10-23 16:39:44 +1100

    add support Visual Studio 2013, 2015,  and 2017

--- src/VsKeyFinder/Data/ProductData.cs
+++ src/VsKeyFinder/Data/ProductData.cs
@@ -10,6 +10,41 @@ namespace VsKeyFinder.Data
             {
                 new Product()
                 {
+                    Name = $"Visual Studio 2013 {Properties.Resources.TxtProfessional}",
+                    Guid = "E79B3F9C-6543-4897-BBA5-5BFB0A02BB5C",
+                    Code = "06177",
+                    Key = ""
+                },
+                new Product()
+                {
+                    Name = $"Visual Studio 2015 {Properties.Resources.TxtEnterprise}",
+                    Guid = "4D8CFBCB-2F6A-4AD2-BABF-10E28F6F2C8F",
+                    Code = "07060",
+                    Key = ""
+                },
+                new Product()
+                {
+                    Name = $"Visual Studio 2015 {Properties.Resources.TxtProfessional}",
+                    Guid = "4D8CFBCB-2F6A-4AD2-BABF-10E28F6F2C8F",
+                    Code = "07062",
+                    Key = ""
+                },
+                new Product()
+                {
+                    Name = $"Visual Studio 2017 {Properties.Resources.TxtEnterprise}",
+                    Guid = "5C505A59-E312-4B89-9508-E162F8150517",
+                    Code = "08860",
+                    Key = ""
+                },
+                new Product()
+                {
+                    Name = $"Visual Studio 2017 {Properties.Resources.TxtProfessional}",
+                    Guid = "5C505A59-E312-4B89-9508-E162F8150517",
+                    Code = "08862",
+                    Key = ""
+                },
+                new Product()
+                {
                     Name = $"Visual Studio 2019 {Properties.Resources.TxtEnterprise}",
                     Guid = "41717607-F34E-432C-A138-A3CFD7E25CDA",
                     Code = "09260",
